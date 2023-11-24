a52d2bf48386b4d67db249a2cc7a9296cfd23f24
Arnold Chris <arnoldchrisoduor@gmail.com>
2023-11-25 00:15:28 +0300

    logistic regression

--- binary_logistic_regression.py
+++ binary_logistic_regression.py
@@ -1,9 +1,9 @@
 import numpy as np
 
 class LogisticRegression:
-    def __init__(self, learning_rate = 0.001, n_inters=1000):
+    def __init__(self, learning_rate = 0.001, n_iters=1000):
         self.lr = learning_rate
-        self.n_iters = n_inters
+        self.n_iters = n_iters
         self.weights = None
         self.bias = None
 
@@ -20,3 +20,46 @@ class LogisticRegression:
             #approximating value of y with linear combination of weights and x, plus bias.
             linear_model = np.dot(X, self.weights) +self.bias
             #applying sigmoid function to the answer.
+            y_predicted = self._sigmoid(linear_model)
+
+            #compute the gradients.
+            dw = (1 / n_samples) * np.dot(X.T, (y_predicted - y))
+            db = (1 / n_samples) * np.sum(y_predicted - y)
+
+            #update parameters.
+            self.weights -= self.lr * dw
+            self.bias -= self.lr *db
+
+    def predict(self, X):
+        linear_model = np.dot(X, self.weights) + self.bias
+        y_predicted = self._sigmoid(linear_model)
+        y_predicted_cls = [1 if i > 0.5 else 0 for i in y_predicted]
+        return np.array(y_predicted_cls)
+
+    def _sigmoid(self, x):
+        return 1 / (1 + np.exp(-x))
+        
+
+    #Testing.
+if __name__ == "__main__":
+
+    from sklearn.model_selection import train_test_split
+    from sklearn import datasets
+
+
+    def accuracy(y_true, y_pred):
+        accuracy = np.sum(y_true == y_pred) / len(y_true)
+        return accuracy
+        
+    bc = datasets.load_breast_cancer()
+    X, y = bc.data, bc.target
+
+    X_train, X_test, y_train, y_test = train_test_split(
+            X, y, test_size=0.2, random_state = 1234
+    )
+
+    regressor = LogisticRegression(learning_rate=0.0001, n_iters=1000)
+    regressor.fit(X_train, y_train)
+    predictions = regressor.predict(X_test)
+
+    print("LR classification accuracy:", accuracy(y_test, predictions))
