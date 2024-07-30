3a5985a6c82c72fe9faa73502516be7eb1e03993
arnoldchrisoduor1 <arnoldchrisoduor@gmail.com>
2024-07-30 16:21:41 +0300

    inverting matrices

---
+++ ML CookBook/Vectors_Matrices_Arrays/inverting_matrices.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+# Create matrix
+matrix_a = np.array([[1, 1],
+[1, 2]])
+# Create matrix
+matrix_b = np.array([[1, 3],
+[1, 2]])
+
+
+# Multiply the two matrices.
+print(np.dot(matrix_a, matrix_b))
