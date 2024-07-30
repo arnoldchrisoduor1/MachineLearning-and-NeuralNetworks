383202c777705ed71ae8832ca2c636a92bc7c076
arnoldchrisoduor1 <arnoldchrisoduor@gmail.com>
2024-07-30 20:25:33 +0300

    loading unstructured data

--- ML CookBook/0x02-Loading-Sample-Data/unstructured_data.py
+++ ML CookBook/0x02-Loading-Sample-Data/unstructured_data.py
@@ -6,9 +6,9 @@ txt_url = "https://machine-learning-python-cookbook.s3.amazonaws.com/text.txt"
 r = requests.get(txt_url)
 # Write it to text.txt locally
 with open('text.txt', 'wb') as f:
-f.write(r.content)
+    f.write(r.content)
 # Read in the file
 with open('text.txt', 'r') as f:
-text = f.read()
+    text = f.read()
 # Print the content
 print(text)
