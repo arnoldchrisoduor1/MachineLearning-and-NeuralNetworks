d9ca00947ba169f1004b6800706c1e4b863be2dd
arnoldchrisoduor1 <arnoldchrisoduor@gmail.com>
2024-07-30 15:44:35 +0300

    creating vectors

---
+++ ML CookBook/Vectors_Matrices_Arrays/create_Vector.py
@@ -0,0 +1,15 @@
+# Using Numpy to create a one-dimensional array:
+
+import numpy as np
+
+# create vector as a row.
+vector_row = np.array([1, 2, 3])
+
+# create vector as a column.
+vector_column = np.array([
+    [1],
+    [2],
+    [3]
+])
+
+# A vector is just an array with a single dimension.
